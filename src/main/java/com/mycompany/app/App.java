c06a470efb2e7a66cc7a3ca6ed05175501ab3eb9
Sandeepgoudgandu <118959767+Sandeepgoudgandu@users.noreply.github.com>
2025-06-24 18:24:27 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello sandeep from saahi systems!";
 
     public App() {}
 
